0bfdc63d4376a191012f6c6c2426f14e7d0b862b
Pakten, Kerem (k20057059) <kerem_ege.pakten@kcl.ac.uk>
2022-12-08 03:31:33 +0000

    ValueError added

--- primes.py
+++ primes.py
@@ -4,11 +4,14 @@
 def primes(number_of_primes):
     list = []
 
-    if number_of_primes > 1:
+    if number_of_primes >= 1:
+        if number_of_primes == 1:
+            list.append(2)
+            return list
         prime_range = int(number_of_primes)**2
         for check_prime in range(2, prime_range):
             primeCounter = 0
-            if len(list) <= number_of_primes:
+            if len(list) < number_of_primes:
                 for j in range(2,prime_range):
                     if check_prime % j == 0:
                         if check_prime != j:
@@ -16,6 +19,8 @@ def primes(number_of_primes):
                         primeCounter+=1
                 if (primeCounter >= 1):
                     list.append(check_prime)
-
-
         return list
+    else:
+        raise ValueError
+
+print(primes(0))
